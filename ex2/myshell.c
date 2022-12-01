73ccebda620a196e0b185bb3a20fd599851c6bbc
guylamdan <guylamdan@mail.tau.ac.il>
2022-12-01 16:21:12 +0200

    almost final

--- ex2/myshell.c
+++ ex2/myshell.c
@@ -8,13 +8,13 @@
 #include <sys/wait.h>
 
 
-enum Process {EXECOMMAND, EXEBACKGROUND, PIPING, REDIRECTING};
+enum Process {EXECOMMAND, EXEBACKGROUND, PIPING, REDIRECTING}; // https://www.geeksforgeeks.org/enumeration-enum-c/
 enum Level {PARENT, CHILD};
 struct sigaction sa;
 
 
-enum Process defineProcess(int count, char**arglist, int *separateLocation){
-    int i = 0;
+enum Process defineProcess(int count, char**arglist, int *separateLocation){ 
+    int i;
 
     if (strcmp(arglist[count - 1],"&") == 0){ // background process case
         return EXEBACKGROUND;
@@ -40,10 +40,9 @@ return EXECOMMAND; // standart case
 
 static void initialize_signals(enum Level level){ // define the required SIGINT behavior 
     if (level == PARENT){
-        signal(SIGINT, SIG_IGN);
+        signal(SIGINT, SIG_IGN); // https://en.cppreference.com/w/c/program/SIG_strategies , 
         return;
     }
-     
         signal(SIGINT, SIG_DFL); // default behavior for child, except background    
 }
 
@@ -55,18 +54,26 @@ static void child_handler(int sig) {
     while((pid = waitpid(-1, &status, WNOHANG)) > 0);
 }
 
-static void spec_waitpid(int pid){
-    if (waitpid(pid, NULL, 0) < 0 && (errno == EINTR || errno != ECHILD)){ // ignore only EINTR and ECHILD errors
+static int spec_waitpid(int pid){
+    if (waitpid(pid, NULL, 0) < 0 && (errno != EINTR && errno != ECHILD)) { // ignore only EINTR and ECHILD errors, https://man7.org/linux/man-pages/man3/errno.3.html
         fprintf(stderr,"Error with waitpid process: %s\n", strerror(errno));
+        return 0;
     }
+
+    return 1;
 }
 
-static void prevent_zombies_handler() {
+static int prevent_zombies_handler() { /// checkkkkkkkkkkkk
     //ERAN'S TRICK
     sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+    sa.sa_flags = SA_RESTART; //| SA_NOCLDSTOP;
     sa.sa_handler = child_handler;
-    sigaction(SIGCHLD, &sa, NULL);
+     if (sigaction(SIGCHLD, &sa, NULL) == -1){ // https://man7.org/linux/man-pages/man2/sigaction.2.html#RETURN_VALUE
+        fprintf(stderr,"Error with waitpid process: %s\n", strerror(errno));
+        exit(1);
+     }
+
+     return 1;
 }
 
 
@@ -76,49 +83,70 @@ int piping(char* commandA, char** commandArgsA, char* commandB, char** commandAr
     int pid_b;
 
     if (pipe(fd) < 0){
-        fprintf(stderr,"Error: %s\n", strerror(errno));
+        fprintf(stderr,"Error with PIPE: %s\n", strerror(errno));
+        return 0;
     }
 
     pid_a = fork();
+    
 
     if (pid_a < 0){
-        fprintf(stderr,"Error: %s\n", strerror(errno));
-        exit(1);
+        fprintf(stderr,"Error with Fork: %s\n", strerror(errno));
+        return 0;
     }
-    
+    /* first command child case */ 
+
     else if (pid_a == 0){
         initialize_signals(CHILD); //don't ignore SIGINT
-        dup2(fd[1],1);
+
+        if (dup2(fd[1],1) == -1){
+            fprintf(stderr,"Error with dup2: %s\n", strerror(errno)); // https://www.mkssoftware.com/docs/man3/dup2.3.asp
+            exit(1);
+        }; // https://www.youtube.com/watch?v=5fnVr-zH-SE
+
         close(fd[0]);
         close(fd[1]);
+
         if (execvp(commandA,commandArgsA) < 0){
             fprintf(stderr,"Error: %s\n", strerror(errno));
             exit(1);
         }
     }
+    /* back tto parent */ 
+
     else { 
         pid_b = fork();
 
         if (pid_b  < 0){
-            fprintf(stderr,"Error: %s\n", strerror(errno));
-            exit(1);
+            fprintf(stderr,"Error with Fork: %s\n", strerror(errno));
+            return 0;
         }
         
+        /* second xommand child case */ 
+
         else if (pid_b == 0){
             initialize_signals(CHILD); // don't ignore sigint
-            dup2(fd[0], 0);
+
+            if (dup2(fd[0], 0) == -1){
+                fprintf(stderr,"Error with dup2: %s\n", strerror(errno)); // https://www.mkssoftware.com/docs/man3/dup2.3.asp
+                exit(1);
+            }
+
             close(fd[1]);
             close(fd[0]);
+
             if (execvp(commandB,commandArgsB) < 0){
                 fprintf(stderr,"Error: %s\n", strerror(errno));
                 exit(1);
             }
-        }        
+        }
+        /* back to parent */ 
+
         else {
             close(fd[0]);
             close(fd[1]);
-            spec_waitpid(pid_a);
-            spec_waitpid(pid_b);
+            return spec_waitpid(pid_a) && spec_waitpid(pid_b);
+            
         }
         
     }
@@ -131,18 +159,31 @@ int redirecting(char* command, char** commandArgs, char* fileName){
     int fd = open(fileName, O_WRONLY|O_CREAT|O_TRUNC, S_IRWXU); // https://man7.org/linux/man-pages/man2/open.2.html, https://man7.org/linux/man-pages/man2/open.2.html
 
     pid = fork();
-    if (pid  == 0){
+
+    if (pid  < 0){
+            fprintf(stderr,"Error with Fork: %s\n", strerror(errno));
+            return 0;
+        }
+
+    else if (pid  == 0){
         initialize_signals(CHILD);
-        dup2(fd, 1); 
+
+        if (dup2(fd, 1) == -1){ // https://www.geeksforgeeks.org/dup-dup2-linux-system-call/
+            fprintf(stderr,"Error with dup2: %s\n", strerror(errno)); // https://www.mkssoftware.com/docs/man3/dup2.3.asp
+            exit(1);
+        }
+
         close(fd);
         if (execvp(command,commandArgs) < 0){
                 fprintf(stderr,"Error: %s\n", strerror(errno));
                 exit(1);
-        }}       
-        else {
-            close(fd);
-            spec_waitpid(pid);
         }
+
+    }       
+    else {
+        close(fd);
+        return spec_waitpid(pid);
+    }
     return 1;
 }
 
@@ -151,18 +192,19 @@ int backGroundProcess(char* command, char** commandArgs){
     pid = fork();
 
     if (pid < 0){
-        fprintf(stderr,"Error: %s\n", strerror(errno));
-        return 1; // terminate the shell (?)
+        fprintf(stderr,"Error with Fork: %s\n", strerror(errno));
+        return 0;
     }
 
     else if (pid == 0) {
         initialize_signals(PARENT); // should not terminate UPON SIGINT like parent behavior
+
         if (execvp(command,commandArgs) < 0){
             fprintf(stderr,"Error: %s\n", strerror(errno));
             exit(1);
         }}
-    prevent_zombies_handler();
-    return 1;
+
+    return prevent_zombies_handler();
 }
 
 int standartProcess(char* command, char** commandArgs){
@@ -176,19 +218,17 @@ int standartProcess(char* command, char** commandArgs){
             initialize_signals(CHILD); // don't ignore SIGINT
             if (execvp(command,commandArgs) < 0){
                 fprintf(stderr,"Error: %s\n", strerror(errno));
-                exit(-1);
+                exit(1);
             }
             exit(1);
             break;
         
-        case -1: // check for -1 or could be something else 
+        case -1: // Failure
             fprintf(stderr,"Error with FORK : %s\n", strerror(errno));
-            exit(-1);
             return 0;
-            break;
         
         default: // parent case
-            spec_waitpid(pid);
+            return spec_waitpid(pid);
                 
     }
 
@@ -207,9 +247,10 @@ int process_arglist(int count, char** arglist){
     char* fileName = NULL;
 
     action = defineProcess(count, arglist, &(separateLocation));
+    commandA = arglist[0];
 
     if (action == PIPING){
-        commandA = arglist[0];
+        // commandA = arglist[0];
         commandB = arglist[separateLocation + 1];
         commandArgsA = arglist;
         commandArgsB= &(arglist[separateLocation + 1]);
@@ -218,7 +259,7 @@ int process_arglist(int count, char** arglist){
     }
 
     else if (action == REDIRECTING){
-        commandA = arglist[0];
+        // commandA = arglist[0];
         arglist[separateLocation] = NULL;
         commandArgsA = arglist;
         fileName = arglist[separateLocation + 1];
@@ -226,24 +267,21 @@ int process_arglist(int count, char** arglist){
     }
 
     else if (action == EXEBACKGROUND){
-        commandA = arglist[0];
+        // commandA = arglist[0];
         arglist[count - 1] = NULL;
         return backGroundProcess(commandA, arglist);
     }
 
     else { 
-         return standartProcess(arglist[0], arglist);
+         return standartProcess(commandA, arglist);
     }
 
     return 1;
 }
 
-
-
 int prepare(void){
-    printf("%d\n", getpid());
     initialize_signals(PARENT);
-    prevent_zombies_handler();
+    // prevent_zombies_handler();
     return 0;
 }
 
